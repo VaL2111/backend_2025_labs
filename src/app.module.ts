00cbddee5cc8fe91df62a361f55eb337accbd004
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-10-30 12:11:06 +0200

    feat: create an .env file and implement its reading

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,39 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { CategoriesModule } from './categories/categories.module';
-import { RecordsModule } from './records/records.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { CategoriesModule } from "./categories/categories.module";
+import { RecordsModule } from "./records/records.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule, CategoriesModule, RecordsModule],
+  imports: [
+    UsersModule,
+    CategoriesModule,
+    RecordsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+
+        host: configService.get<string>("DATABASE_HOST"),
+        port: configService.get<number>("DATABASE_PORT"),
+        username: configService.get<string>("DATABASE_USER"),
+        password: configService.get<string>("DATABASE_PASSWORD"),
+        database: configService.get<string>("DATABASE_NAME"),
+
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
